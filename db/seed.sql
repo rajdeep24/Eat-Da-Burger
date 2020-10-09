ed7666bad699c418ccefe85cbdd7f49d283031bb
Developer Raj <rajdeepkakar@gmail.com>
2020-10-09 00:04:40 -0400

    Initial setup up of all files

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (burger_name) VALUES ('Big MAC');
