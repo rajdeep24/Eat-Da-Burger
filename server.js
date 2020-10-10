dfd10083c6d2d3093f33d5a5b735c046556a43a8
Developer Raj <rajdeepkakar@gmail.com>
2020-10-10 14:04:59 -0400

    Updated the server.js file middleware parameter settings for the urlencoding to true.

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
 // Parse application body
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
