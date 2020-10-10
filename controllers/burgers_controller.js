586e62837e56b372c91c705ef3c7dd433e0c7367
Developer Raj <rajdeepkakar@gmail.com>
2020-10-10 08:36:20 -0400

    Updated the routes that reference the burger object from the object model.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,7 +7,7 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-	burger.all(function (data) {
+	burger.selectAll(function (data) {
 		var hbsObject = {
 			burgers: data,
 		};
@@ -17,7 +17,7 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-	burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
+	burger.createOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
 		// Send back the ID of the new quote
 		res.json({ id: result.insertId });
 	});
@@ -28,7 +28,7 @@ router.put("/api/burgers/:id", function (req, res) {
 
 	console.log("condition", condition);
 
-	burger.update(
+	burger.updateOne(
 		{
 			devoured: req.body.devoured,
 		},
@@ -47,7 +47,7 @@ router.put("/api/burgers/:id", function (req, res) {
 router.delete("/api/burgers/:id", function (req, res) {
 	var condition = "id = " + req.params.id;
 
-	burger.delete(condition, function (result) {
+	burger.deleteOne(condition, function (result) {
 		if (result.affectedRows == 0) {
 			// If no rows were changed, then the ID must not exist, so 404
 			return res.status(404).end();
